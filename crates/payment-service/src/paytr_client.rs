ee7a865aa309283009b85a538f198139310e67ae
Ramazan ERTAN <ramazanertan@gmail.com>
2026-06-25 00:18:18 +0300

    paytr endpoint

--- crates/payment-service/src/paytr_client.rs
+++ crates/payment-service/src/paytr_client.rs
@@ -1,3 +1,3 @@
-pub const PAYTR_PAYMENT_ENDPOINT: &str = "https://www.paytr.com/odeme/api/v1";
+pub const PAYTR_PAYMENT_ENDPOINT: &str = "https://www.paytr.com/odeme";
 pub const PAYTR_CARD_LIST_ENDPOINT: &str = "https://www.paytr.com/odeme/capi/list";
 pub const PAYTR_CARD_DELETE_ENDPOINT: &str = "https://www.paytr.com/odeme/capi/delete";
